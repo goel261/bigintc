8a87eedb59c08fc2a549fdbf789d077dd73af60f
Kshitij Goel <goel261@gmail.com>
2021-04-27 11:46:07 +0530

    TODO

--- bigint.cpp
+++ bigint.cpp
@@ -1,12 +1,56 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class BigInt{
-
+    public:
+    vector<int> v;
+    int sign;
     
+    BigInt(string s){
+        int i = 0;
+        if(s[0]=='-'){
+            sign = 1;
+            i++;
+        }
+        else sign = 0;
+        for(;i<s.size();i++)
+            v.push_back(s[i]-'0');
+    }
+    BigInt(long long int x){
+        if( x < 0 )
+            sign = 1;
+        else sign = 0;
+        x = abs(x);
+        while(x!=0){
+            v.push_back(x%10);
+            x = x/10;
+        }
+        reverse(v.begin(),v.end());
+    }
+    int compare(BigInt x){
+        vector<int> a = x.v;
+        if(a.size()>v.size())
+            return -1;
+        if(a.size() < v.size())
+            return 1;
+        
+        for(int i=0;i<a.size();i++){
+            if(a[i]>v[i])
+                return -1;
+            if(a[i]<v[i])
+                return 1;
+        }
+        // Means that they have equal magnitude
+        return 0;
+    }
+    // TO DO: add, sub, mul, div
+    void add(BigInt a){
+        vector<int> temp = a.v;
 
+    }
 
-}
+};
 
 
 int main(){
